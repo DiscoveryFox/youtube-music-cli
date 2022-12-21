f95b7d0810da3a1a0afa3705e5910c562f1726c9
discovery-bit <61625792+discovery-bit@users.noreply.github.com>
2022-12-22 00:57:40 +0100

    added options to register songs, playlists, confirm them and added a check if the song or the playlist is in the database

--- Attemp_three/database.py
+++ Attemp_three/database.py
@@ -1,4 +1,6 @@
 import sqlite3
+import json
+
 from models.Playlist import Playlist
 from models.Song import Song
 
@@ -26,24 +28,54 @@ class Database:
         self.main_cursor.close()
         self.connection.close()
 
-    def get_connection(self):
-        with self.Connection(database_path) as conn:
-            ...
-
-    def in_database(self, video_id: str):
-        ...
+    def in_database(self, song_id: str) -> bool:
+        with self.Connection(self.database_path) as db:
+            db.cursor.execute('SELECT * FROM Songs WHERE SongID = ? UNION SELECT * FROM Playlists '
+                              'WHERE PlaylistID = ?',
+                              (song_id, song_id))
+            if not db.cursor.fetchall():
+                return False
+            else:
+                return True
 
     def register_song(self, song: Song):
-        ...
+        with self.Connection(self.database_path) as db:
+            db.cursor.execute('INSERT INTO songs (SongID, Name, Filepath, Creator, Downloaded) '
+                              'VALUES (?, ?, ?, ?, ?)',
+                              (song.id, song.name, song.filepath, song.creator, False)
+                              )
+            db.connection.commit()
 
     def register_playlist(self, playlist: Playlist):
-        ...
+        with self.Connection(self.database_path) as db:
+            db.cursor.execute('INSERT INTO Playlists (PlaylistID, Name, Creator, Downloaded, '
+                              'Songs) '
+                              'VALUES (?, ?, ?, ?, ?)',
+                              (playlist.id, playlist.name, playlist.creator, False,
+                               json.dumps(playlist.songs))
+                              )
+            db.connection.commit()
 
     def confirm_song(self, id: str):
-        ...
+        with self.Connection(self.database_path) as db:
+            db.cursor.execute('UPDATE songs SET Downloaded = ? WHERE SongID = ?', (True, id))
+            db.connection.commit()
 
     def confirm_playlist(self, id: str):
-        ...
+        with self.Connection(self.database_path) as db:
+            db.cursor.execute('UPDATE playlists SET Downloaded = ? WHERE PlaylistID = ?',
+                              (True, id))
+            db.connection.commit()
+
+    def list_playlists(self):
+        with self.Connection(self.database_path) as db:
+            db.cursor.execute('SELECT * FROM playlists')
+            return [Playlist(database_tuple=playlist) for playlist in db.cursor.fetchall()]
+
+    def list_songs(self):
+        with self.Connection(self.database_path) as db:
+            db.cursor.execute('SELECT * FROM songs')
+            return [Song(database_tuple=song) for song in db.cursor.fetchall()]
 
     def create_default_layout(self):
         with self.Connection(self.database_path) as conn:
@@ -58,7 +90,7 @@ CREATE TABLE IF NOT EXISTS Playlists (
             ''')
             conn.cursor.execute('''
 CREATE TABLE IF NOT EXISTS Songs (
-    VideoID TEXT PRIMARY KEY NOT NULL,
+    SongID TEXT PRIMARY KEY NOT NULL,
     Name TEXT NOT NULL,
     Filepath TEXT UNIQUE NOT NULL,
     Creator TEXT NOT NULL,
@@ -68,4 +100,6 @@ CREATE TABLE IF NOT EXISTS Songs (
             conn.connection.commit()
 
 
-
+if __name__ == '__main__':
+    x = Database(r'C:\Users\Flinn\Documents\youtube-music-cli\Attempt_one\cache\songs.db')
+    print(x.list_playlists())
