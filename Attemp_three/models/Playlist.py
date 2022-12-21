cdde95b9a650a92a62cba727fb18729742b0acf5
discovery-bit <61625792+discovery-bit@users.noreply.github.com>
2022-12-22 00:56:58 +0100

    added option to convert the database output directly into an object with just supplier the tuple we get from the db request

--- Attemp_three/models/Playlist.py
+++ Attemp_three/models/Playlist.py
@@ -1,11 +1,25 @@
+import json
+
+
 class Playlist:
     name: str
     songs: list[str]
     creator: str
     id: str
 
-    def __init__(self, id: str, name: str, songs: list[str], creator: str):
+    def __init__(self,
+                 id: str = None,
+                 name: str = None,
+                 songs: list[str] = None,
+                 creator: str = None,
+                 database_tuple: tuple = None):
         self.name = name
         self.id = id
         self.songs = songs
         self.creator = creator
+
+        if not all([self.name, self.id, self.creator, self.songs]):
+            self.id, self.name, self.creator, downloaded, self.songs = database_tuple
+            # TODO: Check if there is a nicer way than downloaded maybe like *_ or smth with the
+            #  underscore
+            self.songs: list[str] = json.loads(self.songs)  # NOQA
--- Attemp_three/models/Song.py
+++ Attemp_three/models/Song.py
@@ -1,14 +1,22 @@
 class Song:
+    id: str
     name: str
     filepath: str
     creator: str
-    id: str
 
-    def __init__(self, id: str, name: str, filepath: str, creator: str):
+    def __init__(self,
+                 id: str = None,
+                 name: str = None,
+                 filepath: str = None,
+                 creator: str = None,
+                 database_tuple: tuple = None):
         self.id = id
         self.name = name
         self.filepath = filepath
         self.creator = creator
+
+        if not all([self.id, self.name, self.filepath, self.creator]):
+            self.id, self.name, self.filepath, self.creator, _ = database_tuple
 
     def __str__(self):
         return f'Song {self.name} from {self.creator} with Id: {self.id} with path:' \
